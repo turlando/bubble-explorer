bc2aed7cc01955241171947a37549438b20537c9
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-03-26 20:07:08 +0100

    Add comments to utils functions

--- utils.c
+++ utils.c
@@ -2,16 +2,23 @@
 
 #include <stdlib.h>
 
+/* Return a natural number between zero and max.
+ */
 unsigned int rand_int(unsigned int max) {
     return rand() % (max + 1);
 }
 
+/* Swap the content of two unsigned integers.
+ */
 void swap(unsigned int *a, unsigned int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
+/* Replace the content of each item in the vector v with a random
+ * natural number between zero and max.
+ */
 void vec_init(unsigned int *v, unsigned int size, unsigned int max) {
     for (unsigned int i = 0; i < size; ++i) {
         v[i] = rand_int(max);
