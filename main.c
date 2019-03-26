1eddeae485ccd359f7111f3ee0ec35b43d716f69
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-03-26 11:42:49 +0100

    Remove usage of globals outside main

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 #include "utils.h"
@@ -9,22 +10,23 @@
 #define SPAN     8
 #define DELAY    2
 
-void bubble_sort(unsigned int *vec, unsigned int size) {
-    for (unsigned int i = 0; i < size - 1; i++) {
-        for (unsigned int j = 0; j < size - i - 1; j++) {
-            if (vec[j] > vec[j+1]) {
-                print_vec_swap(vec, VEC_SIZE,
-                               SPAN, VEC_SIZE - i,
+void bubble_sort(unsigned int *vec, unsigned int size,
+                 unsigned int span) {
+    for (unsigned int i = 0; i < size - 1; ++i) {
+        for (unsigned int j = 0; j < size - i - 1; ++j) {
+            if (vec[j] > vec[j + 1]) {
+                print_vec_swap(vec, size,
+                               span, size - i,
                                j, j + 1,
                                "Swap!");
                 swap(vec + j, vec + j + 1);
-                print_vec_swap(vec, VEC_SIZE,
-                               SPAN, VEC_SIZE - i,
+                print_vec_swap(vec, size,
+                               span, size - i,
                                j + 1, j,
                                NULL);
             } else {
-                print_vec_noswap(vec, VEC_SIZE,
-                                 SPAN, VEC_SIZE - i,
+                print_vec_noswap(vec, size,
+                                 span, size - i,
                                  j);
             }
             sleep(DELAY);
@@ -39,7 +41,7 @@ int main(void) {
     vec_init(vec, VEC_SIZE, VAL_MAX);
 
     print_vec_boxed(vec, VEC_SIZE, SPAN);
-    bubble_sort(vec, VEC_SIZE);
+    bubble_sort(vec, VEC_SIZE, SPAN);
     print_vec_boxed(vec, VEC_SIZE, SPAN);
 
     return 0;
